d813d91b577eb973a8fecf1724d5c76a88903c4f
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-06-18 18:45:03 -0500

    20200618: In This commit we can see how use templates and send parameters into a view and how we can show this information

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    name = 'Cristian'
+    course = 'Python'
+    is_premium = False
+    courses = ['Python', 'Ruby', 'Java', 'Elixir']
+    return render_template("index.html", 
+    username=name,course_name=course,
+    is_premium=is_premium,
+    courses=courses
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
